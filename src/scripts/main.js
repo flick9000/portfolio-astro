7261136f7391b96f9e019ff4f98ffeb339f9d63d
Francesco <137506210+flick9000@users.noreply.github.com>
2024-09-20 15:17:22 +0200

    Various Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,18 +11,6 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
-function responsiveNav() {
-  const navbar = document.getElementById("navbar");
-
-  if (!navbar.classList.contains("responsive")) {
-    navbar.classList.add("responsive");
-    document.body.style.overflow = "hidden"; // Or whatever fallback you need
-  } else {
-    navbar.classList.remove("responsive");
-    document.body.style.overflow = "visible";
-  }
-}
-
 function adjustOverflow() {
   if (window.innerWidth > 600) {
     document.body.style.overflow = "visible";
